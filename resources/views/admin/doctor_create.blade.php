a1b91e6bb4c9f14ffb9a70b98217d3179493339e
iammashikur <700mirash@gmail.com>
2022-02-14 21:29:19 +0600

    :white_check_mark:  User Profile

--- resources/views/admin/doctor_create.blade.php
+++ resources/views/admin/doctor_create.blade.php
@@ -87,7 +87,7 @@
                   </div>
 
                   <div class="form-group row mb-4">
-                    <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">BMDC Registration Numer</label>
+                    <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">BMDC Registration Number</label>
                     <div class="col-sm-12 col-md-7">
                       <input type="text" name="registration" class="form-control" required>
                     </div>
--- resources/views/admin/doctor_edit.blade.php
+++ resources/views/admin/doctor_edit.blade.php
@@ -98,7 +98,7 @@ $page_type = 'Admin';
 
                         <div class="form-group row mb-4">
                             <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">BMDC Registration
-                                Numer</label>
+                                Number</label>
                             <div class="col-sm-12 col-md-7">
                                 <input type="text" name="registration" value="{{ $doctor->registration }}"
                                     class="form-control" required>
