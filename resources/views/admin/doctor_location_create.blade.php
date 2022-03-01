7063e7029dfa4d347a8560533808fb3f26d55889
iammashikur <700mirash@gmail.com>
2022-03-01 16:19:10 +0600

    :white_check_mark: Agent and Doctor Commission

---
+++ resources/views/admin/doctor_location_create.blade.php
@@ -0,0 +1,125 @@
+@php
+$page_type = 'Admin';
+@endphp
+@extends('admin.layouts.master')
+
+@push('styles')
+    <link rel="stylesheet" href="{{ url('assets/admin/bundles/jquery-selectric/selectric.css') }}">
+    <link rel="stylesheet" href="{{ url('assets/admin/bundles/bootstrap-tagsinput/dist/bootstrap-tagsinput.css') }}">
+    <link rel="stylesheet" href="{{ url('assets/admin/css/style.css') }}">
+    <link rel="stylesheet" href="{{ url('assets/admin/css/components.css') }}">
+    <style>
+        .col-form-label.text-md-right.col-12.col-md-3.col-lg-3 {
+            font-size: 15px;
+        }
+
+    </style>
+@endpush
+
+@section('content')
+    <div class="main-content">
+        <div class="section">
+            @include('admin.partials.error')
+
+
+            <div class="card card-primary">
+                <div class="card-header" style="border-bottom-color: #d0d0d0">
+                    <h4>Add Location</h4>
+                    <div class="card-header-action">
+                        <a href="{{ route('doctor.index') }}" class="btn btn-warning">Go Back</a>
+                    </div>
+                </div>
+
+                <div class="card-body">
+
+                    <form action="{{ route('doctor-location.store') }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+
+
+                        <div class="form-group row mb-4">
+                            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Doctor</label>
+                            <div class="col-sm-12 col-md-7">
+                                <select class="form-control" name="doctor_id" id="">
+                                    <option> -- select -- </option>
+                                    @foreach (App\Models\Doctor::all() as $item)
+                                        <option value="{{ $item->id }}">{{ $item->name }} </option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-4">
+                            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Hospital</label>
+                            <div class="col-sm-12 col-md-7">
+                                <select class="form-control" name="hospital_id" id="">
+                                    <option> -- select -- </option>
+                                    @foreach (App\Models\Hospital::all() as $item)
+                                        <option value="{{ $item->id }}">{{ $item->name }} </option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+
+
+                        <div class="form-group row mb-4">
+                            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Address</label>
+                            <div class="col-sm-12 col-md-7">
+                                <textarea type="text" name="address" class="form-control" required></textarea>
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-4">
+                            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Room Number</label>
+                            <div class="col-sm-12 col-md-7">
+                                <input type="text" name="room" class="form-control" required>
+                            </div>
+                        </div>
+
+
+
+                        <div class="form-group row mb-4">
+                            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Start Time</label>
+                            <div class="col-sm-12 col-md-7">
+                                <input type="time" name="start_time" class="form-control" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-4">
+                            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">End Time</label>
+                            <div class="col-sm-12 col-md-7">
+                                <input type="time" name="end_time" class="form-control" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-4">
+                            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Consultation Fee</label>
+                            <div class="col-sm-12 col-md-7">
+                                <input type="number" name="consultation_fee" class="form-control" required>
+                            </div>
+                        </div>
+
+
+                        <div class="form-group row mb-4">
+                            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3"></label>
+                            <div class="col-sm-12 col-md-7">
+                                <button class="btn btn-primary">Add Doctor Location</button>
+                            </div>
+                        </div>
+
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('scripts')
+    <script src="{{ url('assets/admin/bundles/jquery-selectric/jquery.selectric.min.js') }}"></script>
+    <script src="{{ url('assets/admin/bundles/upload-preview/assets/js/jquery.uploadPreview.min.js') }}"></script>
+    <script src="{{ url('assets/admin/bundles/bootstrap-tagsinput/dist/bootstrap-tagsinput.min.js') }}"></script>
+    <script src="{{ url('assets/admin/bundles/ckeditor/ckeditor.js') }}"></script>
+    <!-- Page Specific JS File -->
+    <script src="{{ url('assets/admin/js/page/create-post.js') }}"></script>
+    <script src="{{ url('assets/admin/js/page/ckeditor.js') }}"></script>
+@endpush
