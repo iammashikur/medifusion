b30bab4a916ced54cce3403d9bc50d00eb26df5b
iammashikur <700mirash@gmail.com>
2022-03-05 14:13:33 +0600

    :white_check_mark: Doctor location api updated

--- routes/api.php
+++ routes/api.php
@@ -24,27 +24,26 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 
-Route::get('/doctor-by-cat/{id}', [ApiController::class, 'doc_by_cat']);
-Route::get('/doctors', [ApiController::class, 'doctors']);
+// Route::get('/doctor-by-cat/{id}', [ApiController::class, 'doc_by_cat']);
+// Route::get('/doctors', [ApiController::class, 'doctors']);
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::get('/doctor-categories', [ApiController::class, 'doc_cat']);
+    Route::get('/doctor-by-cat/{id}', [ApiController::class, 'doc_by_cat']);
+    Route::get('/doctors', [ApiController::class, 'doctors']);
+    Route::post('/fix-appointment', [ApiController::class, 'fix_appointment']);
+    Route::get('/my-appointments', [ApiController::class, 'my_appointments']);
+    Route::get('/tests', [ApiController::class, 'tests']);
+    Route::get('/test-categories', [ApiController::class, 'test_category']);
+    Route::get('/test-by-cat/{id}', [ApiController::class, 'test_by_cat']);
+    Route::post('/patient-tests', [ApiController::class, 'patient_tests']);
+    Route::get('/my-tests', [ApiController::class, 'my_tests']);
+    Route::post('/update-profile', [ApiController::class, 'update_profile']);
 
 
-// Route::middleware('auth:sanctum')->group(function () {
 
-//     Route::get('/doctor-categories', [ApiController::class, 'doc_cat']);
-//     Route::get('/doctor-by-cat/{id}', [ApiController::class, 'doc_by_cat']);
-//     Route::get('/doctors', [ApiController::class, 'doctors']);
-//     Route::post('/fix-appointment', [ApiController::class, 'fix_appointment']);
-//     Route::get('/my-appointments', [ApiController::class, 'my_appointments']);
-//     Route::get('/tests', [ApiController::class, 'tests']);
-//     Route::get('/test-categories', [ApiController::class, 'test_category']);
-//     Route::get('/test-by-cat/{id}', [ApiController::class, 'test_by_cat']);
-//     Route::post('/patient-tests', [ApiController::class, 'patient_tests']);
-//     Route::get('/my-tests', [ApiController::class, 'my_tests']);
-//     Route::post('/update-profile', [ApiController::class, 'update_profile']);
-
-
-
-// });
+});
 
 
 
