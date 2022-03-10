4ada06afa8634b1e223714b2ed1f2ea0b07c290d
Mashikur Rahman <69344611+iammashikur@users.noreply.github.com>
2022-03-10 14:07:04 +0600

    Footer

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 
 <footer class="main-footer">
   <div class="footer-left">
-    Copyright &copy; 2021 <div class="bullet"></div> Design Develop By <a target="_blank" href="">MASHIKUR</a>
+    Copyright &copy; 2022 <div class="bullet"></div> Developed By <a target="_blank" href="">MASHIKUR</a>
   </div>
   <div class="footer-right">
   </div>
