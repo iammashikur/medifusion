e944a4a2f38521c49d9a9ae3a04076c99034874b
Mashikur Rahman <69344611+iammashikur@users.noreply.github.com>
2022-03-14 01:30:43 +0600

    Bug fix

--- app/DataTables/AgentTestsDataTable.php
+++ app/DataTables/AgentTestsDataTable.php
@@ -28,7 +28,7 @@ class AgentTestsDataTable extends DataTable
         })
 
         ->addColumn('agent', function ($query) {
-            return $query->getAgent->Data ? $query->getAgent->Data->name;
+            return $query->getAgent->Data ? $query->getAgent->Data->name : '';
         })
 
         ->addColumn('status', function ($query) {
