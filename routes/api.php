78381a5395a65f2c8e8dd52f8933e5e3ce6c6d8a
iammashikur <700mirash@gmail.com>
2022-03-19 23:27:26 +0600

    :white_check_mark: Compouner

--- routes/api.php
+++ routes/api.php
@@ -79,7 +79,5 @@ Route::middleware('auth:sanctum')->prefix('agent')->group(function () {
 Route::post('/compounder/login', [AuthController::class, 'compounder_login']);
 
 Route::middleware('auth:sanctum')->prefix('compounder')->group(function () {
-
     Route::post('/logout', [AuthController::class, 'logout']);
-
 });
