2a2a2c8ed4cebc4618789b0afb1e0cf7a7dc35a4
iammashikur <700mirash@gmail.com>
2022-04-01 17:01:38 +0600

    :white_check_mark: Cancel appointment

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -43,6 +43,55 @@
                   </a>
               </div>
 
+
+              <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                <a href="">
+                    <div class="card">
+                        <div class="card-statistic-4">
+                          <div class="align-items-center justify-content-between">
+                            <div class="row ">
+                              <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
+                                <div class="card-content">
+                                  <h5 class="font-12">Agents</h5>
+                                  <h2 class="mb-3 font-18">{{ App\Models\Agent::count() }}</h2>
+                                </div>
+                              </div>
+                              <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0 d-flex align-items-center justify-content-center text-center">
+                                <div class="banner-img">
+                                  <img style="max-width: 55px" class="" src="{{ asset('uploads/icons/agent.png') }}" alt="">
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                <a href="">
+                    <div class="card">
+                        <div class="card-statistic-4">
+                          <div class="align-items-center justify-content-between">
+                            <div class="row ">
+                              <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
+                                <div class="card-content">
+                                  <h5 class="font-12">Hospital</h5>
+                                  <h2 class="mb-3 font-18">{{ App\Models\Hospital::count() }}</h2>
+                                </div>
+                              </div>
+                              <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0 d-flex align-items-center justify-content-center text-center">
+                                <div class="banner-img">
+                                  <img style="max-width: 55px" class="" src="{{ asset('uploads/icons/hospital.png') }}" alt="">
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
               <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-xs-12">
                   <a href="">
                       <div class="card">
