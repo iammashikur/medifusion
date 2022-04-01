8b06399dd51707c43141839fe87fa82f0ebd4acb
iammashikur <700mirash@gmail.com>
2022-04-01 16:51:16 +0600

    :white_check_mark: Cancel appointment

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -140,7 +140,7 @@
                                 <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
                                   <div class="card-content">
                                     <h5 class="font-12">Appointments</h5>
-                                    <h2 class="mb-3 font-18">{{ $appointments->where(['status_id'=> 5, 'by_agent' => null])->count() }}</h2>
+                                    <h2 class="mb-3 font-18">{{ $appointments->where(['status_id'=> 5, 'by_agent' => 0])->count() }}</h2>
                                   </div>
                                 </div>
                                 <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0 d-flex align-items-center justify-content-center text-center">
@@ -179,7 +179,6 @@
                 </a>
             </div>
 
-
               <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-xs-12">
                   <a href="">
                       <div class="card">
@@ -189,7 +188,7 @@
                                 <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
                                   <div class="card-content">
                                     <h5 class="font-12">User Tests</h5>
-                                    <h2 class="mb-3 font-18">{{ $usertest->where(['status_id' => 2, 'by_agent' => null])->count() }}</h2>
+                                    <h2 class="mb-3 font-18">{{ $usertest->where(['status_id' => 2, 'by_agent' => 1])->count() }}</h2>
                                   </div>
                                 </div>
                                 <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0 d-flex align-items-center justify-content-center text-center">
