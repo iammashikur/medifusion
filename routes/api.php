19cd5bf088dbccacb83b08317656141cfee9879f
iammashikur <700mirash@gmail.com>
2022-05-06 10:41:38 +0600

    :white_check_mark: Compounder Api

--- routes/api.php
+++ routes/api.php
@@ -98,8 +98,8 @@ Route::middleware('auth:sanctum')->prefix('compounder')->group(function () {
     Route::get('/hospitals',[ApiController::class, 'compounder_hospitals']);
     Route::get('/appointments/{id}',[ApiController::class, 'compounder_appointments']);
     Route::get('/tests/{id}',[ApiController::class, 'compounder_tests']);
-    Route::post('/update-appointment/{id}',[ApiController::class, 'compounder_appointment_update']);
-    Route::post('/update-test/{id}',[ApiController::class, 'compounder_test_update']);
+    Route::post('/update-appointment',[ApiController::class, 'compounder_appointment_update']);
+    Route::post('/update-test',[ApiController::class, 'compounder_test_update']);
 
     Route::post('/logout', [AuthController::class, 'logout']);
 
