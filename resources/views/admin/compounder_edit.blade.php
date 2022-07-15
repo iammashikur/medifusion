c63609cc6f65527a7ab3c513acbe889638398cc1
Mashikur Rahman <69344611+iammashikur@users.noreply.github.com>
2022-07-15 12:11:55 +0600

    compounder password

--- resources/views/admin/compounder_edit.blade.php
+++ resources/views/admin/compounder_edit.blade.php
@@ -134,7 +134,7 @@ $page_title = 'Edit Compounder';
                         <div class="form-group row mb-4">
                             <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Password (Optional)</label>
                             <div class="col-sm-12 col-md-7">
-                                <input type="text" name="password" class="form-control">
+                                <input type="text" name="password" autocomplete="false" class="form-control">
                             </div>
                         </div>
 
